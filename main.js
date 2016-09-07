7ce99c1135c70cd8076687cff0a6d51283b30ff2
Michail <osimike96@gmail.com>
2016-09-07 20:20:03 +0800

    added new web page called cat

---
+++ main.js
@@ -0,0 +1,24 @@
+var count = 50;
+var i = count;
+var timerId = setInterval(function () {
+    textCoordinate.textContent = i + ";";
+    i--;
+    if (i == 0) {
+        textCoordinate.innerHTML = "";
+        paint.style.display = "block";
+        clearInterval(timerId);
+    }
+}, count);
+//textCoordinate.textContent = "Done!";
+//function displayCoordinates(event) {
+//    textCoordinate.textContent = event.clientX + ";" + event.clientY;
+//    console.log(event.clientX + ";" + event.clientY);
+//}
+////heh.onmousemove = displayCoordinates;
+//myBody.addEventListener('mousemove', displayCoordinates);
+function handler(event) {
+    paint.style.marginLeft = event.clientX - +paint.style.width.slice(0, -2) / 2;
+    paint.style.marginTop = event.clientY - +paint.style.width.slice(0, -2) / 2;
+    paint.style.width = (+paint.style.width.slice(0, -2) + 5) + "px";
+}
+myBody.addEventListener('mousemove', handler);
